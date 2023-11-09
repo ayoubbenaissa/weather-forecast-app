bf6237d4f9ce6689b14db0694cd30f1039ea660e
ayoubbenaissa <ayoub.benaissa.mail@gmail.com>
2023-11-09 08:07:19 +0100

    improve styling (for mobile)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     <div className="home-page">
       <div className="home-page_content">
         <div className="home-welcome">
-          Welcome to &nbsp;<div className="app-name">Weather Forecast App</div>
+          <div className="welcome">Welcome to &nbsp;</div><div className="app-name">Weather Forecast App</div>
         </div>
         <div className="home-description">
           To see weather forecast for the upcoming 5 days, navigate to <Link className="link-forecast-page" to="/forecast">Forecast Page</Link>
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 
+import "../styles/NoPage.scss";
+
 export const NoPage = () => {
   return (
     <div className="page-not-found">
-      <h2 className="page-not-found_header">Oops, seems that you are in the wrong page!</h2>
-      <div className="page-not-found_info">
-        Try and go back to&nbsp;
-        <Link className="home-link" to="/">
-          Home page
-        </Link>
+      <div className="page-not-found_content">
+        <h2 className="page-not-found_header">Oops! seems that you are in the wrong page</h2>
+        <div className="page-not-found_info">
+          Try and go back to&nbsp;
+          <Link className="home-link" to="/">
+            Home page
+          </Link>
+        </div>
       </div>
     </div>
   );
